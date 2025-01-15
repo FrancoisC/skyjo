e6447ee8bc47c327049e5fdf91547f13aa610f49
François Chotard <frachotard@gmail.com>
2025-01-15 15:30:43 +0100

    Init repo

---
+++ src/main/java/io/github/francoisc/skyjo/SkyjoApplication.java
@@ -0,0 +1,13 @@
+package io.github.francoisc.skyjo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyjoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyjoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/francoisc/skyjo/SkyjoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.francoisc.skyjo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyjoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
